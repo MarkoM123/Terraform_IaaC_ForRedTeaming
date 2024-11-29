9a7216f3aae399b3129d2eb658dff584b879a672
markinjo <94840102+markinjo@users.noreply.github.com>
2024-11-29 22:01:24 +0100

    providers.tf

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+}
+provider "aws" {
+  region                   = "eu-central-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
