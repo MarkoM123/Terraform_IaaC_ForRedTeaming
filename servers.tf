2356734d0d91dbcb4dee055c750b96e1d7ee1dc4
markinjo <94840102+markinjo@users.noreply.github.com>
2024-11-29 22:02:09 +0100

    servers.tf

--- servers.tf
+++ servers.tf
@@ -1,28 +1,34 @@
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
 resource "aws_instance" "c2_server" {
-  ami           = "ami-12345678" # Zameni stvarnim AMI ID
+  ami           = "ami-0084a47cc718c111a"
   instance_type = "t2.micro"
-  subnet_id     = var.public_subnet_id
-  key_name      = var.key_name
+  subnet_id     = aws_subnet.public.id
+  key_name      = aws_key_pair.my_key.key_name
   tags = {
     Name = "c2-server"
   }
 }
 
 resource "aws_instance" "redirector" {
-  ami           = "ami-12345678" # Zameni stvarnim AMI ID
+  ami           = "ami-0084a47cc718c111a"
   instance_type = "t2.micro"
-  subnet_id     = var.public_subnet_id
-  key_name      = var.key_name
+  subnet_id     = aws_subnet.public.id
+  key_name      = aws_key_pair.my_key.key_name
   tags = {
     Name = "redirector"
   }
 }
 
 resource "aws_instance" "main_server" {
-  ami           = "ami-12345678" # Zameni stvarnim AMI ID
+  ami           = "ami-0084a47cc718c111a"
   instance_type = "t2.micro"
-  subnet_id     = var.private_subnet_id
-  key_name      = var.key_name
+  subnet_id     = aws_subnet.public.id
+  key_name      = aws_key_pair.my_key.key_name
   tags = {
     Name = "main-server"
   }
