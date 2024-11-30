7dc681ee4588df87769db298c93f205fcfca416d
Marko Milinkovic <markomilinkovic10@gmail.com>
2024-11-30 13:04:28 +0100

    Dodani serveri novi

--- servers.tf
+++ servers.tf
@@ -7,6 +7,15 @@ resource "aws_key_pair" "my_key" {
 resource "aws_instance" "c2_server" {
   ami           = "ami-0084a47cc718c111a"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+  key_name      = aws_key_pair.my_key.key_name
+  tags = {
+    Name = "c2-server"
+  }
+}
+resource "aws_instance" "c2_server_redirector" {
+  ami           = "ami-0084a47cc718c111a"
+  instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   key_name      = aws_key_pair.my_key.key_name
   tags = {
@@ -14,22 +23,43 @@ resource "aws_instance" "c2_server" {
   }
 }
 
-resource "aws_instance" "redirector" {
+
+resource "aws_instance" "payload_redirector" {
   ami           = "ami-0084a47cc718c111a"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   key_name      = aws_key_pair.my_key.key_name
   tags = {
-    Name = "redirector"
+    Name = "payload_redirector"
+  }
+}
+resource "aws_instance" "payload" {
+  ami           = "ami-0084a47cc718c111a"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+  key_name      = aws_key_pair.my_key.key_name
+  tags = {
+    Name = "payload"
   }
 }
 
-resource "aws_instance" "main_server" {
+
+resource "aws_instance" "phishing_server" {
+  ami           = "ami-0084a47cc718c111a"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+  key_name      = aws_key_pair.my_key.key_name
+  tags = {
+    Name = "Phishing"
+  }
+}
+
+resource "aws_instance" "phishing_server_redirector" {
   ami           = "ami-0084a47cc718c111a"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   key_name      = aws_key_pair.my_key.key_name
   tags = {
-    Name = "main-server"
+    Name = "SMTP"
   }
 }
