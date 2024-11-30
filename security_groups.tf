4488bda0fab8b3a7fd99c3f12455d62619ce02eb
Marko Milinkovic <markomilinkovic10@gmail.com>
2024-11-30 13:06:29 +0100

    Dodani serveri novi

--- security_groups.tf
+++ security_groups.tf
@@ -2,12 +2,20 @@ resource "aws_security_group" "c2_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    description = "Allow HTTP/HTTPS"
+    description = "Allow HTTP"
     from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    description = "Allow HTTP"
+    from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 
   ingress {
     description = "Allow SSH"
@@ -30,8 +38,15 @@ resource "aws_security_group" "redirector_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    description = "Allow HTTP/HTTPS"
+    description = "Allow HTTP"
     from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
